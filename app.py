fa68fe3cc4ba67d4ba54b38e491e502a698cb5c2
Ankit-Ransh <ankitanand2125@gmail.com>
2023-05-01 00:38:20 +0530

    Added sessions to prevent default logout

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, url_for, redirect, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "$rtu&&3420@erWXVY"
+app.permanent_session_lifetime = timedelta(minutes = 60)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,34 +18,55 @@ class users(db.Model):
 
 @app.route('/')
 def login():
+
+    if email in session:
+        if email == "":
+            return render_template('login.html')
+
+    if 'email' in session:
+        return redirect(url_for('home'))
+
     return render_template('login.html')
 
 
 @app.route('/login_validation', methods=['POST', 'GET'])
 def login_validation():
     if (request.method == "POST"):
+    
         email = request.form['email']
         password = request.form['password']
-
+    
         login = users.query.filter_by(email=email, password=password).first()
 
         if email == "":
+            session.pop('_flashes', None)
             flash('Email cannot be empty')
             return render_template("login.html")
         
         if password == "":
+            session.pop('_flashes', None)
             flash('Password cannot be empty')
             return render_template("login.html")
 
         if login is not None:
+            # check if session exists
+            session.pop('_flashes', None)
             flash('Successful Login')
-            return redirect(url_for('home'))
+            session['email'] = email
+            session.permanent = True
+            return redirect(url_for('email'))
+            
         else:
+            session.pop('_flashes', None)
             flash('Account do not exist')
             return render_template('login.html')
-        
-    return render_template('login.html')
+    
+    else:
+        if 'email' in session:
+            return redirect(url_for('email'))
 
+        return render_template('login.html')
+        
 
 @app.route('/register')
 def register():
@@ -58,11 +81,13 @@ def registration_validation():
 
         # email cannot be empty
         if email == "":
+            session.pop('_flashes', None)
             flash('Email cannot be empty')
             return render_template("register.html")
         
         # password cannot be empty
         if password == "":
+            session.pop('_flashes', None)
             flash('Password cannot be empty')
             return render_template("register.html")
     
@@ -71,10 +96,12 @@ def registration_validation():
         is_email_present = users.query.filter_by(email=email).first()
 
         if is_email_present is not None:
+            session.pop('_flashes', None)
             flash('Email already exists')
             return render_template("register.html")
 
         if password != confirm_password:
+            session.pop('_flashes', None)
             flash('Passwords do not match')
             return render_template('register.html')
         
@@ -84,6 +111,8 @@ def registration_validation():
         if password == confirm_password:
             db.session.add(register)
             db.session.commit()
+            session.pop('_flashes', None)
+            flash('Account created Successfully')
             return redirect(url_for('login'))
         else:
             return render_template('register.html')
@@ -92,20 +121,26 @@ def registration_validation():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    if 'email' in session:
+        return render_template('home.html')
+    else:
+        return render_template('login.html')
 
-# @app.route('/logout')
-# def logout():
-#     return redirect(url_for('login'))
+@app.route('/email')
+def email():
+    if 'email' in session:
+        session.pop('_flashes', None)
+        flash('Successfully logged in')
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html')
 
-# @app.route('/user')
-# def user():
-#     if "email_id" in session:
-#         email_id = session["email_id"]
-#         return redirect('home')
-#     else:
-#         return redirect('login')
-
+@app.route('/logout')
+def logout():
+    session.pop('email',None)
+    session.pop('_flashes', None)
+    flash('Logged out successfully')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
